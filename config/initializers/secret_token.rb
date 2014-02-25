b7efa4b33627e1687784f7b057689564e0b0e5cb
LilJohnnyBlackFlag <liljohnnyblackflag@liljohnnyblackflag-1015E.(none)>
2014-02-25 01:53:57 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AutoDelegate::Application.config.secret_key_base = 'be8a1a9afdcc58a6afdd4186d29dcc54f4eb190e42e705e488ea6968abc483a10cb9bb7eea322bfec3115dcace4c7e7034786f26be3879ffaa5fef8dd290d090'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AutoDelegate::Application.config.session_store :cookie_store, key: '_auto_delegate_session'
